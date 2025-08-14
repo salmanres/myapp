08c3af87577173dc2d8caa357ef22bf87448c388
salman_siddeeque <salmanres@gmail.com>
2025-08-14 18:59:01 +0530

    change

--- src/dashboard/LandingPage.js
+++ src/dashboard/LandingPage.js
@@ -5,6 +5,7 @@ function LandingPage(){
         <Fragment>
             <h1>landing page...</h1>
             <h1>landing page 2nd heading</h1>
+            <h1>landing page 3rd heading</h1>
         </Fragment>
     )
 }
