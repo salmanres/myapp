cdca0d9f38fa7af9087e1056cab00a778f668a4d
salman_siddeeque <salmanres@gmail.com>
2025-09-24 19:17:27 +0530

    change

--- src/dashboard/UserData.js
+++ src/dashboard/UserData.js
@@ -1,10 +1,68 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
+import axios from 'axios';
 
 function UserDataApi() {
-  
+
+    const [userData, setUserData] = useState([]);
+
+    const getdata = async () => {
+        try {
+            const response = await axios.get('http://localhost:4500/userdata');
+            console.log(response.data);
+            setUserData(response.data);
+        } catch (err) {
+            console.log(err);
+        };
+    };
+
+    const deletedata = async (id) => {
+        try {
+            const result = window.confirm('are you sure?');
+            if (result == true) {
+                const response = await axios.delete(`http://localhost:4500/deleteuser/${id}`);
+                alert(response.data);
+                getdata();
+            }else{
+                alert('cancelled!');
+            };
+        } catch (err) {
+            console.log(err);
+        };
+    };
+
+    useEffect(() => {
+        getdata();
+    }, []);
+
     return (
         <Fragment>
-           <h1>user data</h1>
+            <h1>user data</h1>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">username</th>
+                        <th scope="col">email</th>
+                        <th scope="col">password</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {userData.length > 0 ? userData.map((item, index) => (
+                        <tr>
+                            <td>{index + 1}</td>
+                            <td>{item.username}</td>
+                            <td>{item.email}</td>
+                            <td>{item.password}</td>
+                            <td>
+                                <button type="button" class="btn btn-warning btn-sm">DETAILS</button>
+                                <button type="button" class="btn btn-danger btn-sm mx-2">EDIT</button>
+                                <button type="button" class="btn btn-secondary btn-sm" onClick={() => deletedata(item._id)}>DELETE</button>
+                            </td>
+                        </tr>
+                    )) : <p>loading..</p>}
+                </tbody>
+            </table>
         </Fragment>
     )
 }
